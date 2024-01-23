204f116ab58ff85c4cc1611082f5165ab9203f8c
minkyu1180 <minkyu1180@naver.com>
2024-01-23 21:18:46 +0900

    web FE 0123

---
+++ 리액트 기초부터 쇼핑몰 프로젝트까지/FE/web/src/datas/users.js
@@ -0,0 +1,20 @@
+// src/datas/users.js
+// 회원 정보
+let users = [
+    {
+        id: 0,
+        userId: "minkyu1180",
+        userPwd: "yj680408",
+        userName: "김민규",
+        isStaff: true,
+    },
+    {
+        id: 1,
+        userId: "als1655",
+        userPwd: "yj680408",
+        userName: "이민규",
+        isStaff: false,
+    }
+]
+
+export default users;
