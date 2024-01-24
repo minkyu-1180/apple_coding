7387aa8604323db54d7c7bc87f64f81f6ca27d94
minkyu-1180 <minkyu1180@naver.com>
2024-01-24 17:38:55 +0900

    코딩애플 shop ~mapping Component 0124

--- 리액트 기초부터 쇼핑몰 프로젝트까지/실습/shop/src/App.js
+++ 리액트 기초부터 쇼핑몰 프로젝트까지/실습/shop/src/App.js
@@ -1,44 +1,69 @@
-// 사용할 리액트 부트스트랩 모듈 불러오기
-import 'bootstrap/dist/css/bootstrap.css'; // css적용하는 법
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React, { useState } from 'react';
+// react bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav, Row, Col, Image } from 'react-bootstrap';
+// css
 import './App.css';
-import ProductList from './Components/ProductList';
-import { Routes, Route, Link } from 'react-router-dom';
+import bg from './images/bg.png';
 
+// 내부 컴포넌트
+// src/datas/shoes.js에서 만들어 놓은 shoes 데이터 가져다 쓰기
+import shoes from './datas/shoes.js';
 
-function Home() {
+// Col 부분을 카드 컴포넌트로 바꾸기
+function Card (props) {
   return (
-    // 상품 레이아웃 만들기
-    <Container>
-      <ProductList />
-    </Container>
-  );
+      <Col md={4}>
+          <Image src={process.env.PUBLIC_URL + props.shoe.imageUrl} width="80%" />
+          <h3>상품 이름 : {props.shoe.title}</h3>
+          <p>상품 설명 : {props.shoe.content}</p>
+          <p>상품 가격 : {props.shoe.price}</p>
+      </Col>
+  )
 }
 
 function App() {
+  // 앞에서 가져온 데이터 가져다 쓰기!
+  let [shoeList, setShoeList] = useState(shoes);
+  // console.log(shoeList)
+  // 상품 추가
+  function addShoe (shoe) {
+    let newShoeList = [...shoeList];
+    newShoeList.push(shoe);
+    setShoeList(newShoeList);
+  }
+
   return (
     <div className="App">
-      {/* <Button variant="primary">Primary</Button> */}
-      <Navbar bg="light" variant="light">
+      {/* 네비게이션 바 */}
+      <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">민규 샾</Navbar.Brand>
+          <Navbar.Brand href="#home">민규의신발</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>|  
-            <Nav.Link href="#features">Cart</Nav.Link>| 
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      {/* 메인 이미지 */}
+      <div className="main-bg" style={{ backgroundImage: 'url('+ bg +')'}}></div>
 
-      {/* 링크 보여주기 */}
-      <Link to="/">홈페이지</Link>
-      <Link to="/detail">상세 페이지</Link>
-      {/* 라우터 링크 */}
-      <Routes>
-        {/* 메인 페이지에서만 보여주고자 하는 컴포넌트 : Home */}
-        <Route path="/" element={<Home />} />
-        <Route path="/detail" element={<div>상세 페이지</div>} />
-        <Route path="/about" element={<div>어바웃 페이지</div>} />
-      </Routes>
+      {/* 상품 진열 컨테이너*/}
+      <Container>
+        <Row>
+          {/* mapping 해버렷! */}
+          { shoeList.map((shoe, index) => (
+            <Card 
+              // 매핑 시 키값 부여 잊지 맙시다
+              key={shoe.id}
+              index={index}
+              shoe={shoe}
+            />
+          )
+          )}
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ 리액트 기초부터 쇼핑몰 프로젝트까지/실습/shop/src/datas/shoes.js
@@ -0,0 +1,29 @@
+// src/datas/shoes.js
+
+let shoes = [
+    {
+      id : 0,
+      imageUrl: '/images/shoes1.jpg',
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      imageUrl: '/images/shoes2.jpg',
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      imageUrl: '/images/shoes3.jpg',
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+  ] 
+
+export default shoes;
